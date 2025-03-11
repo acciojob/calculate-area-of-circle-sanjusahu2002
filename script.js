7515e2ca1e65c7b5f8bf27842519454c842164bf
sanjusahu2002 <132829320+sanjusahu2002@users.noreply.github.com>
2025-03-11 09:30:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,17 @@
             // Get the radius from the user using the prompt() function
             let radius = prompt("Enter the radius of the circle:");
             // write you code here and display the result to the user
-           
+                   let radiusElement = document.getElementById('radius');
+        let radius = prompt("enter radiyes");
+        let cRadius = parseInt(radius);
+        if(cRadius>0){
+            
+            let area = (3.14*(cRadius*cRadius)).toFixed(2);
+            radiusElement.innerText = `The area of the circle with radius ${cRadius} is ${area}
+            `;
+        }
+        else{
+            radiusElement.innerText = `Enter correct radius.`;
+        }
 }
 calculateArea();
